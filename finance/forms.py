b665dfc2371d58d80d5af213596144a530c163fa
Naresh Saini <yogeshsainins100@gmail.com>
2025-11-02 14:55:09 +0530

    all file added

---
+++ finance/forms.py
@@ -0,0 +1,22 @@
+from django import forms
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
+from finance.models import Transcation,Goal
+
+class RegisterForm(UserCreationForm):
+    class Meta:
+        model = User
+        fields = ['username', 'email', 'password1', 'password2']
+
+
+class TransactionForm(forms.ModelForm):
+    class Meta:
+        model=Transcation
+        fields=['title','amount','transaction_type','date','category']
+
+
+
+class GoalForm(forms.ModelForm):
+    class Meta:
+        model = Goal
+        fields = ['name', 'target_amount','current_amount', 'deadline']
